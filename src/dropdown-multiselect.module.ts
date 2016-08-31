5cbdb5fa93c319ebf8b10aa20bd704422491ca18
Andy Mepham <andymeps@yahoo.com>
2016-08-31 09:27:31 +0100

    Temporary resolution to DROPDOWN_DIRECTIVES

--- src/dropdown-multiselect.module.ts
+++ src/dropdown-multiselect.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 import { DropdownMultiselectComponent } from './components';
 
 @NgModule({
     imports: [ CommonModule ],
-    declarations: [ DropdownMultiselectComponent ],
-    exports: [ DropdownMultiselectComponent ]
+    declarations: [ DropdownMultiselectComponent, DROPDOWN_DIRECTIVES ],
+    exports: [ DropdownMultiselectComponent, DROPDOWN_DIRECTIVES ]
 })
 export class DropdownMultiselectModule { }

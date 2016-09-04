ec641c88f1f96002ea4f5f5c5fad7eaa3d5bd019
Andy Mepham <andymeps@yahoo.com>
2016-09-04 21:11:19 +0100

    Upgraded Angular 2 to rc.6, upgraded ng2-bootstrap to 1.1.1 * Changed dropdown-multiselect.module to support modular version of ng2-bootstrap.

--- src/dropdown-multiselect.module.ts
+++ src/dropdown-multiselect.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import { DropdownModule } from 'ng2-bootstrap';
 import { DropdownMultiselectComponent } from './components';
 
 /**
@@ -11,8 +11,8 @@ import { DropdownMultiselectComponent } from './components';
  * @class DropdownMultiselectModule
  */
 @NgModule({
-    imports: [ CommonModule ],
-    declarations: [ DropdownMultiselectComponent, DROPDOWN_DIRECTIVES ],
-    exports: [ DropdownMultiselectComponent, DROPDOWN_DIRECTIVES ]
+    imports: [ CommonModule, DropdownModule ],
+    declarations: [ DropdownMultiselectComponent ],
+    exports: [ DropdownMultiselectComponent ]
 })
 export class DropdownMultiselectModule { }
